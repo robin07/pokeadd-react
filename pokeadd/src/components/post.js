d5e9d4ddd5a91559ab02e44d1cb77e28434ad6a2
robin07 <robbiesingh.singh84@gmail.com>
2020-09-07 00:30:25 +0530

    updated home and post

--- pokeadd/src/components/post.js
+++ pokeadd/src/components/post.js
@@ -1,19 +1,33 @@
 import React from 'react';
+import axios from 'axios';
 
-class Post extends React.Component{
-    state={
-        id:null
+class Post extends React.Component {
+    state = {
+        post: null
     }
-    componentDidMount(){
-        let id =this.props.match.params.post_id;
-        this.setState({
-            id:id
-        })
+    componentDidMount() {
+        let id = this.props.match.params.post_id;
+        axios.get('https://jsonplaceholder.typicode.com/posts/' + id)
+            .then(res => {
+                console.log(res);
+                this.setState({
+                    post: res.data
+                })
+            })
+
     }
-    render(){
-        return(
-            <div>
-            <h1>{this.state.id}</h1>
+    render() {
+        const post = this.state.post ? (
+            <div className="post">
+                <h4 className="center">{this.state.post.title}</h4>
+                <p>{this.state.post.body}</p>
+            </div>
+        ) : (
+                <div className="center">Loading . . .</div>
+            )
+        return (
+            <div className="container">
+               {post}
             </div>
         )
     }
