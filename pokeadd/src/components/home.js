a5601e8ae11d1289c12408ac6973d2b2de840269
robin07 <robbiesingh.singh84@gmail.com>
2020-09-07 22:03:29 +0530

    connected redux with component

--- pokeadd/src/components/home.js
+++ pokeadd/src/components/home.js
@@ -1,22 +1,13 @@
 import React from 'react';
-import axios from 'axios';
+//import axios from 'axios';
 import { Link } from 'react-router-dom';
 import Pokeball from '../pokeball.png';
+import { connect } from 'react-redux';
 
 class Home extends React.Component {
-    state = {
-        posts: []
-    }
-    componentDidMount() {
-        axios.get('https://jsonplaceholder.typicode.com/posts')
-            .then(res => {
-                this.setState({
-                    posts: res.data.slice(0, 10)
-                })
-            })
-    }
     render() {
-        const { posts } = this.state;
+        console.log(this.props);
+        const { posts } = this.props;
         const postList = posts.length ? (
             posts.map(post => {
                 return (
@@ -42,4 +33,11 @@ class Home extends React.Component {
         )
     }
 }
-export default Home;
+
+const mapStateToProps = (state) => {
+    return {
+        posts: state.posts
+    }
+}
+
+export default connect(mapStateToProps)(Home);
